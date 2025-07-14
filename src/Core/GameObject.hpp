89d3540da6a2ddf843ccbe3f4c929f874a06e826
HEROWL <hiroakitogawa74@gmail.com>
2025-07-14 13:56:07 +0900

    現在進捗写真追加

--- src/Core/GameObject.hpp
+++ src/Core/GameObject.hpp
@@ -56,6 +56,7 @@ namespace Engine::Core
 	//=========================================================
 	class Transform : public Component
 	{
+	public:
 		Transform() = default;
 		~Transform() = default;
 
