81519e5ff6e99d936e0a25e8a1c071bcdf555203
HEROWL <hiroakitogawa74@gmail.com>
2025-07-15 23:35:39 +0900

    RenderComponent作成（Triangle&Cube）

---
+++ src/Graphics/RenderComponent.cpp
@@ -0,0 +1,15 @@
+// src/Graphics/RenderComponent.cpp
+#include "RenderComponent.hpp"
+#include <algorithm>
+
+namespace Engine::Graphics
+{
+	//========================================
+	//RenderComponentŽÀ‘•
+	//========================================
+	RenderComponent::RenderComponent(RenderableType type)
+		:m_renderableType(type)
+	{ }
+
+
+}
